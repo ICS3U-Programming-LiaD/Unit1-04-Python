cbae81bbbac48b3018a1683813e26cc291c78ec3
Lia Duggan <lia.duggan@stu.ocsb.ca>
2022-09-16 18:14:24 +0000

    Area and Perimeter of a Rectangle

---
+++ area_per_rect.py
@@ -0,0 +1,18 @@
+
+#!/usr/bin/env python3
+
+# Created by:Lia Duggan
+# Created on:September 16th 2022
+# This program calculates and displays the area and perimeter of a 
+# rectangle with length 5cm and width 3cm.
+
+def main():
+    print ("For a rectangle that has a dimension")
+    print ("length of 9cm and width 5cm:")
+    print ("")
+    print ("Area = {} cm^2". format (9*5))
+    print ("Perimeter = {} cm". format (2*(9+5)))
+    print()
+  
+if __name__ == "__main__":
+  main()
