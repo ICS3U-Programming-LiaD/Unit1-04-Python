9c67ee038f35dcbd09800df61420d884ef176111
Lia Duggan <lia.duggan@stu.ocsb.ca>
2022-09-16 18:12:11 +0000

    Area and Perimeter of a Rectangle

---
+++ basic_math.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/env python3
+# Created By: Lia Duggan
+# Date: September 16th, 2022
+# This program does basic math
+
+def main():
+    print ("7 + 13 = {}". format(7+13))
+    print ("27 - 9 = {}". format(27-9))
+    print ("9 / 3 = {}". format(9/3))
+    print ("6 - 7 / 2 = {}". format(6-7/2))
+    print ("7^3 = {}". format(7**3))
+    print ("5 / 2 = {}". format(5/2))
+    print()
+
+if __name__ == "__main__":
+    main()
